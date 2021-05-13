4768ddd92c09f23680bded201e1c49027492e6a6
ajhudson <andyhudson1979@gmail.com>
2021-05-13 22:37:41 +0100

    added routing

---
+++ src/components/home/home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => (<p>This is home</p>);
+
+export default Home;
---
+++ src/components/test/test.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Test = () => (<p>This is test</p>);
+
+export default Test;
